0c10cf1887f4514a7f2ad1b9280857a686b1316b
BRUNO EDUARDO FREIRES DA SILVA <bruno.efsilva3@senacsp.edu.br>
2024-07-01 11:56:29 -0300

    rogerio

--- TudoAcabaEmPizza/FrmPedido.Designer.cs
+++ TudoAcabaEmPizza/FrmPedido.Designer.cs
@@ -28,12 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmPedido";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            txtProduto = new TextBox();
+            label2 = new Label();
+            btnInserir = new Button();
+            btnExcluir = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.HotTrack;
+            label1.Location = new Point(284, 13);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(434, 46);
+            label1.TabIndex = 0;
+            label1.Text = "Tudo Acaba em Pizza!";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(284, 94);
+            textBox1.Margin = new Padding(4, 4, 4, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(224, 29);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(577, 94);
+            textBox2.Margin = new Padding(4, 4, 4, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(250, 29);
+            textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(881, 94);
+            textBox3.Margin = new Padding(4, 4, 4, 4);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(224, 29);
+            textBox3.TabIndex = 1;
+            textBox3.TextChanged += textBox2_TextChanged;
+            // 
+            // txtProduto
+            // 
+            txtProduto.Location = new Point(284, 196);
+            txtProduto.Margin = new Padding(4, 4, 4, 4);
+            txtProduto.Name = "txtProduto";
+            txtProduto.Size = new Size(543, 29);
+            txtProduto.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.HotTrack;
+            label2.Location = new Point(284, 171);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Produto";
+            label2.Click += label2_Click;
+            // 
+            // btnInserir
+            // 
+            btnInserir.Location = new Point(881, 196);
+            btnInserir.Name = "btnInserir";
+            btnInserir.Size = new Size(106, 29);
+            btnInserir.TabIndex = 3;
+            btnInserir.Text = "Inserir";
+            btnInserir.UseVisualStyleBackColor = true;
+            btnInserir.Click += button1_Click;
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.Location = new Point(999, 195);
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.Size = new Size(106, 30);
+            btnExcluir.TabIndex = 3;
+            btnExcluir.Text = "Excluir ";
+            btnExcluir.UseVisualStyleBackColor = true;
+            btnExcluir.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = SystemColors.HotTrack;
+            label3.Location = new Point(284, 69);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(104, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Data pedido";
+            label3.Click += label2_Click;
+            // 
+            // FrmPedido
+            // 
+            AutoScaleDimensions = new SizeF(10F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(1143, 630);
+            Controls.Add(btnExcluir);
+            Controls.Add(btnInserir);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(txtProduto);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ForeColor = SystemColors.HotTrack;
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "FrmPedido";
+            Text = "FrmPedido";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox txtProduto;
+        private Label label2;
+        private Button btnInserir;
+        private Button btnExcluir;
+        private Label label3;
     }
 }
