feb4603129a7d660f0e212de269d89bb9c73bf5e
MATHEUS SILVA BERLOFFE <berloffematheus@gmail.com>
2024-07-04 11:53:57 -0300

    alterações nos formularios

--- TudoAcabaEmPizza/FrmNivel.Designer.cs
+++ TudoAcabaEmPizza/FrmNivel.Designer.cs
@@ -36,7 +36,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Name = "FrmNivel";
-            Text = "fmrNivel";
+            Text = "FrmNivel";
             Load += fmrNivel_Load;
             ResumeLayout(false);
         }
